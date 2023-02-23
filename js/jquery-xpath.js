337bfa3f06c39e09b4122a54c256bae4af7c86b2
Yuhau Lin <lyuhau@gmail.com>
2023-02-23 13:46:03 -0500

    jquery-xpath.js: Maybe improve compatibility

--- js/jquery-xpath.js
+++ js/jquery-xpath.js
@@ -1,14 +1,19 @@
-const $ = jQuery;
-$.fn.x = function(xpath) {
-    const result = [];
-    this.each((i, e) => {
-        const xpathResult = document.evaluate(xpath, e, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
+Object.entries((() => {
+    const $ = jQuery;
 
-        var elem;
-        while (elem = xpathResult.iterateNext()) {
-            result.push(elem);
-        }
-    });
-    return $(result);
-};
-const $x = (xpath, context) => $(context ? context : document).x(xpath);
+    $.fn.x = function (xpath) {
+        const result = [];
+        this.each((i, e) => {
+            const xpathResult = document.evaluate(xpath, e, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
+
+            let elem;
+            while (elem = xpathResult.iterateNext()) {
+                result.push(elem);
+            }
+        });
+        return $(result);
+    };
+    const $x = (xpath, context) => $(context ? context : document).x(xpath);
+
+    return {"$x": $x};
+})()).forEach(([k, v]) => window[k] = v);
